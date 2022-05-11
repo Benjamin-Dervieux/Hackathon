146ce399390ef0a73398f7da43e708355925ce0f
Dervieux <dervieux.ben@gmail.com>
2022-05-11 18:43:26 +0200

    initialisation repo react + routes pretes

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+
+export default function Main() {
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </main>
+  );
+}
