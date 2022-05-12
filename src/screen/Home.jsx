04604a211d9829769517f0773804fae3d5f89530
CLément <clmentmoreno@hotmail.com>
2022-05-12 16:35:22 +0200

    stylized the box

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './home.css';
 import Point from '../components/Point';
+import Slide from '../components/Slide';
 
 function Home() {
   return (
     <div>
       <div>
         <Point />
+        <Slide />
       </div>
     </div>
   );
