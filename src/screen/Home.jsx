cdad69704cd0916efdba029b11181c753f8e6616
QuentinJOUANNE <QUENTIN.JOUANNE94@gmail.com>
2022-05-12 16:56:37 +0200

    update

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './home.css';
 import Point from '../components/Point';
+import DisplayMap from '../components/DisplayMap';
+import useLocalStorage from 'use-local-storage';
 
-function Home() {
+function Home({ position }) {
+  const [currentTrace, setcurrentTrace] = useLocalStorage('currentTrace', [
+    [position.lon, position.lat],
+  ]);
+
+  useEffect(() => {
+    let interval = null;
+
+    interval = setInterval(() => {
+      setcurrentTrace([...currentTrace, [position.lon, position.lat]]);
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
   return (
     <div>
       <div>
         <Point />
+        <DisplayMap currentTrace={currentTrace} position={position} zoom={20} />
       </div>
     </div>
   );
