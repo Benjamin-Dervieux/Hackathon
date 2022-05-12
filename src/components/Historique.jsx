b8f54357974a7c9d66524efdd5b70d47542fc3df
RayaneArroudj <rayane.arroudj@gmail.com>
2022-05-12 22:09:27 +0200

    historique ok

--- src/components/Historique.jsx
+++ src/components/Historique.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import styles from './historique.module.css';
 import HistoriqueModale from './HistoriqueModale';
 import Modale from './Modale';
+import { TiArrowUpThick } from 'react-icons/ti';
 
 function Historique() {
   const { revele, toggle } = HistoriqueModale();
 
   return (
-    <div>
-      <button onClick={toggle}>Historique</button>
+    <div className={styles.historique}>
+      <button className={styles.open} type="button" onClick={toggle}>
+        <TiArrowUpThick
+          style={{
+            height: '25px',
+            marginBottom: '5px',
+            width: '50px',
+          }}
+        />
+      </button>
       <Modale revele={revele} cache={toggle} />
     </div>
   );
--- src/components/Modale.jsx
+++ src/components/Modale.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import styles from './historique.module.css';
+import HistoriqueInfos from './HistoriqueInfos';
+import { TiArrowDownThick } from 'react-icons/ti';
 
 const Modale = ({ revele, cache }) =>
   revele ? (
     <React.Fragment>
-      <div>
-        <button type="button" onClick={cache}>
-          <span>&times;</span>
-        </button>
+      <div className={styles.overlay} />
+      <div className={styles.wrapper}>
+        <div className={styles.modale}>
+          <button className={styles.close} type="button" onClick={cache}>
+            <TiArrowDownThick
+              style={{
+                height: '25px',
+                marginBottom: '5px',
+                width: '50px',
+              }}
+            />
+          </button>
+        </div>
         <div>
-          <p>voici la Modale</p>
+          <HistoriqueInfos
+            data={[
+              { distance: 100, carbon: 500, point: 500 },
+              { distance: 100, carbon: 500, point: 500 },
+              { distance: 100, carbon: 500, point: 500 },
+            ]}
+          />
         </div>
       </div>
     </React.Fragment>
